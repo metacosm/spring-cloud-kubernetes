6e99ab1797e767e4f2c8b5e024cfa39a051d759c
Chris Laprun <metacosm@gmail.com>
2017-08-01 17:06:55 +0200

    #116: Initial work to remove hardcoding of application.* property names.

--- spring-cloud-kubernetes-config/src/main/java/org/springframework/cloud/kubernetes/config/ConfigMapPropertySource.java
+++ spring-cloud-kubernetes-config/src/main/java/org/springframework/cloud/kubernetes/config/ConfigMapPropertySource.java
@@ -17,37 +17,116 @@
 
 package org.springframework.cloud.kubernetes.config;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.client.KubernetesClient;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.config.YamlProcessor;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.yaml.SpringProfileDocumentMatcher;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.util.StringUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ConfigMapPropertySource extends MapPropertySource {
     private static final Log LOG = LogFactory.getLog(ConfigMapPropertySource.class);
 
-    private static final String APPLICATION_YML = "application.yml";
-    private static final String APPLICATION_YAML = "application.yaml";
-    private static final String APPLICATION_PROPERTIES = "application.properties";
+    private static final String SCK_CONFIG_NAME = "spring.cloud.kubernetes.config.name";
+    private static final String SPRING_CONFIG_LOCATION = "spring.config.location";
+    private static final String SPRING_CONFIG_NAME = "spring.config.name";
+
+    private static final Set<String> CONFIG_FILES_PREFIXES = new HashSet<>();
+
+    static {
+        CONFIG_FILES_PREFIXES.add("application");
+
+        // first use SCK config name if available
+        String configName = System.getProperty(SCK_CONFIG_NAME);
+        if(configName != null) {
+            LOG.debug(SCK_CONFIG_NAME + "=" + configName);
+            CONFIG_FILES_PREFIXES.add(configName);
+        }
+
+        // if Spring config name is set, add it to prefixes to look for as well
+        configName = System.getProperty(SPRING_CONFIG_NAME);
+        if (configName != null) {
+            LOG.debug(SPRING_CONFIG_NAME + "=" + configName);
+            CONFIG_FILES_PREFIXES.add(configName);
+        }
+    }
+
+    private abstract static class SpringProperty {
+
+        private final String name;
+
+        private SpringProperty(String name) {
+            this.name = name;
+        }
+
+        static SpringProperty from(String fullName) {
+            final SpringProperty defaultProp = new SpringProperty(fullName) {
+                @Override
+                Function<String, Map<String, String>> mapExtractorFor(String[] profiles) {
+                    return s -> Collections.singletonMap(fullName, s);
+                }
+            };
+
+            // first check if the property matches
+            final String name = StringUtils.stripFilenameExtension(fullName);
+            if (!CONFIG_FILES_PREFIXES.contains(name)) {
+                return defaultProp;
+            } else {
+                final String extension = StringUtils.getFilenameExtension(fullName);
+                switch (extension) {
+                    case "yml":
+                    case "yaml":
+                        return new SpringProperty(fullName) {
+                            @Override
+                            Function<String, Map<String, String>> mapExtractorFor(String[] profiles) {
+                                return yamlParserGenerator(profiles).andThen(PROPERTIES_TO_MAP);
+                            }
+                        };
+                    case "properties":
+                        return new SpringProperty(fullName) {
+                            @Override
+                            Function<String, Map<String, String>> mapExtractorFor(String[] profiles) {
+                                return KEY_VALUE_TO_PROPERTIES.andThen(PROPERTIES_TO_MAP);
+                            }
+                        };
+                    default:
+                        return defaultProp;
+                }
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            SpringProperty that = (SpringProperty) o;
+
+            return name.equals(that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+        abstract Function<String, Map<String, String>> mapExtractorFor(String[] profiles);
+    }
 
     private static final String PREFIX = "configmap";
 
-	public ConfigMapPropertySource(KubernetesClient client, String name) {
-		this(client, name, null);
-	}
+    public ConfigMapPropertySource(KubernetesClient client, String name) {
+        this(client, name, null);
+    }
 
     public ConfigMapPropertySource(KubernetesClient client, String name, String[] profiles) {
         this(client, name, null, profiles);
@@ -71,20 +150,17 @@ public class ConfigMapPropertySource extends MapPropertySource {
         Map<String, String> result = new HashMap<>();
         try {
             ConfigMap map = namespace == null || namespace.isEmpty()
-                    ? client.configMaps().withName(name).get()
-                    : client.configMaps().inNamespace(namespace).withName(name).get();
+                ? client.configMaps().withName(name).get()
+                : client.configMaps().inNamespace(namespace).withName(name).get();
 
             if (map != null) {
                 for (Map.Entry<String, String> entry : map.getData().entrySet()) {
                     String key = entry.getKey();
                     String value = entry.getValue();
-                    if (key.equals(APPLICATION_YAML) || key.equals(APPLICATION_YML)) {
-                        result.putAll(yamlParserGenerator(profiles).andThen(PROPERTIES_TO_MAP).apply(value));
-                    } else if (key.equals(APPLICATION_PROPERTIES)) {
-                        result.putAll(KEY_VALUE_TO_PROPERTIES.andThen(PROPERTIES_TO_MAP).apply(value));
-                    } else {
-                        result.put(key, value);
-                    }
+                    final Map<String, String> propertiesAsMap = SpringProperty.from(key).mapExtractorFor(profiles)
+                        .apply(value);
+                    LOG.debug(key + " => " + propertiesAsMap);
+                    result.putAll(propertiesAsMap);
                 }
             }
         } catch (Exception e) {
@@ -95,22 +171,22 @@ public class ConfigMapPropertySource extends MapPropertySource {
 
     private static Map<String, Object> asObjectMap(Map<String, String> source) {
         return source.entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            .stream()
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-	private static final Function<String, Properties> yamlParserGenerator(final String[] profiles) {
-		return s -> {
-			YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
-			if (profiles == null) {
-				yamlFactory.setDocumentMatchers(new SpringProfileDocumentMatcher());
-			} else {
-				yamlFactory.setDocumentMatchers(new SpringProfileDocumentMatcher(profiles));
-			}
-			yamlFactory.setResources(new ByteArrayResource(s.getBytes()));
-			return yamlFactory.getObject();
-		};
-	}
+    private static final Function<String, Properties> yamlParserGenerator(final String[] profiles) {
+        return s -> {
+            YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
+            if (profiles == null) {
+                yamlFactory.setDocumentMatchers(new SpringProfileDocumentMatcher());
+            } else {
+                yamlFactory.setDocumentMatchers(new SpringProfileDocumentMatcher(profiles));
+            }
+            yamlFactory.setResources(new ByteArrayResource(s.getBytes()));
+            return yamlFactory.getObject();
+        };
+    }
 
     private static final Function<String, Properties> KEY_VALUE_TO_PROPERTIES = s -> {
         Properties properties = new Properties();
@@ -122,10 +198,10 @@ public class ConfigMapPropertySource extends MapPropertySource {
         }
     };
 
-    private static final Function<Properties, Map<String,String>> PROPERTIES_TO_MAP = p -> p.entrySet().stream()
-            .collect(Collectors.toMap(
-                    e -> String.valueOf(e.getKey()),
-                    e -> String.valueOf(e.getValue())));
+    private static final Function<Properties, Map<String, String>> PROPERTIES_TO_MAP = p -> p.entrySet().stream()
+        .collect(Collectors.toMap(
+            e -> String.valueOf(e.getKey()),
+            e -> String.valueOf(e.getValue())));
 
 
 }
